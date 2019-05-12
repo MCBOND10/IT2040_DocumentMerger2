ae5f593d3c48df674f46afc8b3014a8e53665391
Matt Bonderer <mcbonderer10@gmail.com>
2019-05-12 13:27:44 -0500

    document merger 2 test run

---
+++ DocumentMerger2/DocumentMerger2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DocumentMerger2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 3) //if improper num of files exist, do this
+            {
+                Console.WriteLine("");
+                Console.WriteLine("DOCUMENT MERGER 2 \nFormat: <input_file_1> <input_file_2> ... <input_file_n> <output_file>");
+                Console.WriteLine("\nSupply at least 2 text files to merge. Following the input files, include a name for the resulting merged text file.");
+                Console.WriteLine("NOTE: Dont forget to add file extensions! ex file1.txt");
+                Console.WriteLine("");
+            }
+            else //if proper num of files exist, do this
+            {
+                string[] files = new string[args.Length - 1];
+                var mfile = args[args.Length - 1];
+                StreamWriter createfile = new StreamWriter(mfile);
+
+                long count = 0;
+                string newfilepath;
+
+                foreach(string filepath in files)
+                {
+                    newfilepath = filepath;
+                    StreamReader readfile = new StreamReader(newfilepath);
+                    string text = null;
+                    while((text = readfile.ReadLine()) != null)
+                    {
+                        createfile.WriteLine(text);
+                        count = count + (long)text.Length;
+                    }
+                    readfile.Close();
+                }
+
+                Console.WriteLine("Your file '{0}' was saved. The document contains {1} characters", mfile, count);
+            }
+        }
+    }
+}
